ff93646c4f32d0b09d45dcfc250c8c71f3bd7edf
Klosowsky <mateuszklos2000@interia.pl>
2023-05-21 12:59:59 +0200

    Basic functionalities of hero controller done

--- backend/src/main/java/com/to/backend/controller/HeroController.java
+++ backend/src/main/java/com/to/backend/controller/HeroController.java
@@ -5,10 +5,7 @@ import com.to.backend.factory.ArcherFactory;
 import com.to.backend.factory.HeroFactory;
 import com.to.backend.factory.MageFactory;
 import com.to.backend.factory.WarriorFactory;
-import com.to.backend.model.CreateHeroRequestBody;
-import com.to.backend.model.Hero;
-import com.to.backend.model.HeroProxy;
-import com.to.backend.model.Warrior;
+import com.to.backend.model.*;
 import com.to.backend.services.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/hero")
@@ -30,13 +29,10 @@ public class HeroController {
     }
 
     @RequestMapping("/save")
-    public ResponseEntity<Hero> save(@RequestBody HeroProxy heroProxy){
+    public ResponseEntity<Hero> saveOrUpdate(@RequestBody HeroProxy heroProxy){
         try{
-            System.out.println("ee jestem tu");
             Hero hero = heroProxy.toValidHero();
-            System.out.println("ee jestem tu2");
             heroService.saveHero(hero);
-            System.out.println("ee jestem tu3");
             return ResponseEntity.ok(hero);
         }catch(Exception ex){
             System.out.println("ex" +ex.getMessage());
@@ -67,7 +63,34 @@ public class HeroController {
         }catch(Exception ex){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+    }
 
+
+    @RequestMapping("/gethero")
+    public ResponseEntity<Hero> getHero(@RequestBody Hero heroRequest){
+        try{
+            return ResponseEntity.ok(heroService.findById(heroRequest.getHeroId()));
+        }catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @RequestMapping("/removehero")
+    public ResponseEntity<Hero> removeHero(@RequestBody Hero heroRequest){
+        try{
+            return ResponseEntity.ok(heroService.removeHero(heroRequest.getHeroId()));
+        }catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @RequestMapping("/getallheroes")
+    public ResponseEntity<List<Hero>> getAllHeroes(){
+        try{
+            return ResponseEntity.ok(heroService.getAllHeroes());
+        }catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
 
--- backend/src/main/java/com/to/backend/services/HeroService.java
+++ backend/src/main/java/com/to/backend/services/HeroService.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class HeroService {
 
@@ -17,8 +20,24 @@ public class HeroService {
     }
 
     public Hero saveHero(Hero hero){
-
         return heroRepository.save(hero);
+    }
+
+    public Hero removeHero(long id){
+        Hero hero = findById(id);
+        heroRepository.deleteById(id);
+        return hero;
+    }
+
+    public Hero findById(long id){
+        return heroRepository.findById(id).orElseThrow();
+    }
+
+    public List<Hero> getAllHeroes(){
+        Iterable<Hero> heroes = heroRepository.findAll();
+        List<Hero> heroesList = new ArrayList<>();
+        heroes.forEach(hero -> heroesList.add(findById(hero.getHeroId())));
+        return heroesList;
     }
 
 
